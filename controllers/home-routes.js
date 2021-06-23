6913f6173bfd1f00bc01776beeb1df2c67109556
anjubimal <anjubimal@tamu.edu>
2021-06-23 12:25:46 -0400

    login

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,9 +1,10 @@
-const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
-
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment, Vote } = require('../models');
 
+// get all posts for homepage
 router.get('/', (req, res) => {
+    console.log('======================');
     Post.findAll({
         attributes: [
             'id',
@@ -28,10 +29,9 @@ router.get('/', (req, res) => {
         ]
     })
         .then(dbPostData => {
-            // pass a single post object into the homepage template
-            console.log(dbPostData[0]);
-            const posts = dbPostData.map(post => post.get({plain:true}))
-            res.render('homepage',{posts});
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+
+            res.render('homepage', { posts });
         })
         .catch(err => {
             console.log(err);
@@ -39,5 +39,13 @@ router.get('/', (req, res) => {
         });
 });
 
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
+});
 
 module.exports = router;
